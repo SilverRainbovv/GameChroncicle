a0c7855da9a21b8df612046e546883cd214ccf5c
SilverRainbovv <vadimdidenko156@gmail.com>
2024-08-07 18:47:11 +0300

    init eureka discovery service

--- ViewService/src/main/java/com/didenko/viewservice/PageController.java
+++ ViewService/src/main/java/com/didenko/viewservice/PageController.java
@@ -1,4 +1,4 @@
-package com.didenko.frontendcontroller;
+package com.didenko.viewservice;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
