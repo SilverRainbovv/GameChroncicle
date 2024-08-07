8df03c7bd9c2dbbfdc5b35b0a5619857a419ab59
SilverRainbovv <vadimdidenko156@gmail.com>
2024-08-07 12:10:14 +0300

    change FrontendController to FrontendService

--- FrontendService/src/main/java/com/didenko/frontendcontroller/FrontendServiceApplication.java
+++ FrontendService/src/main/java/com/didenko/frontendcontroller/FrontendServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FrontendControllerApplication {
+public class FrontendServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FrontendControllerApplication.class, args);
+        SpringApplication.run(FrontendServiceApplication.class, args);
     }
 
 }
--- FrontendService/src/test/java/com/didenko/frontendcontroller/FrontendServiceApplicationTests.java
+++ FrontendService/src/test/java/com/didenko/frontendcontroller/FrontendServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FrontendControllerApplicationTests {
+class FrontendServiceApplicationTests {
 
     @Test
     void contextLoads() {
